0dfca981b745508ab872e572916cf28c5a523594
MartinAteliom <martin@ateliom.fr>
2023-04-23 16:08:18 +0200

    init shoppingcart store

---
+++ martin_airweb/src/stores/shopping-cart-store.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+
+export const useShoppingCartStore = defineStore('shoppingCart', {
+  state: () => ({
+    shoppingCart: [],
+  }),
+  getters: {
+    totalPrice: (state) => {
+      const totalprice = 12344;
+      return totalprice;
+    },
+  },
+  actions: {
+    addTicketToCart(ticket: any) {},
+  },
+});
