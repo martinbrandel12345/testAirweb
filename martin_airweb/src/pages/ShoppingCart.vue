e8742441d64988a9670bb93d8ae54e14f2e3534d
MartinAteliom <martin@ateliom.fr>
2023-04-23 16:45:46 +0200

    ajout du panier

--- martin_airweb/src/pages/ShoppingCart.vue
+++ martin_airweb/src/pages/ShoppingCart.vue
@@ -1,18 +1,57 @@
 <template>
-  <q-page class=""> SHOPPING </q-page>
+  <q-page class="">
+    <div style="width: 95%; margin: 0 auto">
+      <q-card
+        flat
+        style="max-width: 600px; margin: 20px auto"
+        class="bg-secondary q-pa-xs text-white"
+      >
+        <q-card-section class="row items-center q-ml-md">
+          <q-icon size="25px" name="shopping_cart"></q-icon>
+          <h6 class="q-ma-none q-pl-md">Votre panier</h6>
+        </q-card-section>
+
+        <q-card-section
+          v-for="ticket in store.shoppingCart"
+          :key="ticket"
+          class="row justify-between items-center"
+        >
+          <div>
+            <p>{{ ticket.ticket.label }}</p>
+          </div>
+
+          <div class="row justify-between items-center">
+            {{ ticket.quantity }}
+          </div>
+          <div>
+            <p>{{ ticket.ticket.price / 100 }} €</p>
+          </div>
+        </q-card-section>
+        <q-card-section class="row justify-between items-center">
+          <p>TOTAL</p>
+          <p>155 €</p>
+        </q-card-section>
+        <q-card-actions align="right" class="q-mr-sm">
+          <q-btn> ACCEDER AU PAIEMENT</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { useShoppingCartStore } from 'src/stores/shopping-cart-store';
+import { defineComponent, onMounted, reactive } from 'vue';
 
 export default defineComponent({
   name: 'ShoppingCart',
   setup() {
-    const state = reactive({
-      productList: [],
+    const store = useShoppingCartStore();
+    onMounted(() => {
+      console.log(store.shoppingCart);
     });
 
-    return { state };
+    return { store };
   },
 });
 </script>
